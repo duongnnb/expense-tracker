4b262f75411e0ca97fa580557403971fb4ceda1e
Duong <duongminh>
2021-05-23 21:05:42 +0700

    add base component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className='App'>Expense tracker</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </div>
+  );
 }
 
 export default App;
